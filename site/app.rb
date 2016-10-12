f24724a11c1f21502e47584e14cac79381e52e7a
David Newbury <david.newbury@gmail.com>
2016-10-12 09:59:30 -0400

    added docs, content negotiation

--- site/app.rb
+++ site/app.rb
@@ -7,30 +7,27 @@ require 'sinatra/base'
 require "sinatra/reloader" 
 require "sinatra/json"
 
+# Linked Data Libraries
 require 'sparql/client'
-
 require "linkeddata"
 
-
+# Internal Libraries
 require_relative "../lib/getty_query.rb"
 
-
-# # Internal Libraries
-# require "./lib/plant_uml_encode64.rb"
-# require "./lib/aac.rb"
 
 class MyApp < Sinatra::Base
 
 
+  #  Development-Environment-specific configuration
   configure :development do
     register Sinatra::Reloader
     Dir.glob('./lib/**/*') { |file| also_reload file}
     set :show_exceptions, :after_handler
   end
 
+  # Global configuration
   configure do 
     set :getty_sparql, SPARQL::Client.new("http://vocab.getty.edu/sparql")
-
     set :context, File.read("data/context.json")
     set :frame, File.read("data/frame.json")
   end
@@ -44,9 +41,21 @@ class MyApp < Sinatra::Base
   # Index Route
   #----------------------------------------------------------------------------
   get "/" do
-    "TODO:  Write Instructions Here."
+    "
+    <h1> Entity Lookup Service </h1>
+    <p> This project will provide basic RDF information about an entity. Currently, it only supports the Getty Vocabularies, but additional services will soon be added.
+    <h2> Example URLs </h2>
+    <ul>
+      <li> <a href='/getty/500012368.json'> /getty/500012368.json (as JSON-LD)</a>
+      <li> <a href='/getty/500012368.rdf'> /getty/500012368.rdf  (as RDF/XML)</a>
+      <li> <a href='/getty/500012368.ttl'> /getty/500012368.ttl (As Turtle)</a>
+      <li> <a href='/getty/500012368'> /getty/500012368 (using content negotiation)</a>
+    </ul>
+    "
   end
 
+  # The JSON-LD Context
+  #----------------------------------------------------------------------------
   get "/context" do
     cache_control :public
     etag Digest::SHA1.hexdigest(settings.context)
@@ -60,6 +69,8 @@ class MyApp < Sinatra::Base
     settings.context
   end
 
+  # The JSON-LD Frame
+  #----------------------------------------------------------------------------
   get "/frame" do
     cache_control :public
     etag Digest::SHA1.hexdigest(settings.frame)
@@ -74,26 +85,64 @@ class MyApp < Sinatra::Base
   end
 
 
-  get "/getty/:id" do
+  # A lookup for a Getty ID
+  #----------------------------------------------------------------------------
+  get "/getty/:id.?:format?" do
 
     cache_control :public, max_age: 86400
 
     querier = GettyQuery.new(settings.getty_sparql)
 
-    ### THE RIGHT WAY TO DO IT
-    # graph = querier.get_graph(params[:id])
-    # unframed_json = JSON::LD::API::fromRdf(graph)
-    # frame = JSON.parse(settings.frame)
-    # json_results = JSON::LD::API.frame(unframed_json, frame)
-    # result = json_results["@graph"]
+    case params[:format]
 
-    ### THE WRONG WAY TO DO IT
-    result = querier.get_obj(params[:id])
+    # Handle Content Negotiation
+    when nil
+      request.accept.each do |accept_obj|
+        case accept_obj.to_s
+        when"application/ld+json", "application/json"
+          redirect to("#{request.url}.json"), 303
+        when "text/turtle"
+          redirect to("#{request.url}.ttl"), 303
+        when "application/rdf+xml"
+          redirect to("#{request.url}.rdf"), 303
+        end
+      end
+      # default to JSON
+      redirect to("#{request.url}.json"), 303
+    
 
-    etag Digest::SHA1.hexdigest(result.to_json)
-    headers "Link" => "<http://#{request.host_with_port}/context>; rel=\"http://www.w3.org/ns/json-ld#context\"; type=\"application/ld+json\""
+    # Handle JSON-LD
+    when "json"  
 
-    json result
+      ### THE RIGHT WAY TO DO IT
+      # graph = querier.get_graph(params[:id])
+      # unframed_json = JSON::LD::API::fromRdf(graph)
+      # frame = JSON.parse(settings.frame)
+      # json_results = JSON::LD::API.frame(unframed_json, frame)
+      # result = json_results["@graph"]
+      # 
+      ### THE WRONG WAY TO DO IT
+      result = querier.get_obj(params[:id])
+
+      headers "Link" => "<http://#{request.host_with_port}/context>; rel=\"http://www.w3.org/ns/json-ld#context\"; type=\"application/ld+json\""
+      json result
+    
+    # Handle Turtle
+    when "ttl"
+      content_type "text/turtle"
+      graph = querier.get_graph(params[:id])
+      graph.dump(:turtle)  
+
+    # Handle RDF/XML  
+    when "rdf"
+      content_type "application/rdf+xml"
+      graph = querier.get_graph(params[:id])
+      graph.dump(:rdfxml)         
+    end
+
+
+
+
   end
 
 end
